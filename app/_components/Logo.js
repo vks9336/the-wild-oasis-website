5c00d60755a328358ed3d4a80c86b4a739e459ac
vishal33.singh <t-vishal33.singh@ocltp.com>
2025-11-08 20:49:58 +0530

    changes

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import logo from '@/public/logo.png';
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <Image
-        src="/logo.png"
-        height="60"
-        width="60"
+        src={logo}
+        height={60}
+        width={60}
         alt="The Wild Oasis logo"
         priority
+        quality={100}
       />
       <span className="text-xl font-semibold text-[#D4DEE7]">
         The Wild Oasis
